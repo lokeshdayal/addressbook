611061bed28e0f3dcf4733950bdbfec34d86869d
lokeshdayal <lokesh.dayal2009@gmail.com>
2018-09-16 11:46:14 +0530

    Update AddressbookUI.java

--- src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
@@ -71,7 +71,7 @@ public class AddressbookUI extends UI {
          */
         newContact.addClickListener(e -> contactForm.edit(new Contact()));
 
-        filter.setInputPrompt("Addressbook Release V7.2- latest Change");
+        filter.setInputPrompt("Addressbook Release V8.0- latest Change");
         filter.addTextChangeListener(e -> refreshContacts(e.getText()));
 
         contactList
