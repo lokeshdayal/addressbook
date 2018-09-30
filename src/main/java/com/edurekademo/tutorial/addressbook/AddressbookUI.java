5adaf5d3e6716e508f2aae576a729b253f6b661c
lokeshdayal <lokesh.dayal2009@gmail.com>
2018-09-30 10:16:04 +0530

    Update AddressbookUI.java

--- src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
@@ -71,7 +71,7 @@ public class AddressbookUI extends UI {
          */
         newContact.addClickListener(e -> contactForm.edit(new Contact()));
 
-        filter.setInputPrompt("Addressbook Release V15.0- LokeshDayal Website");
+        filter.setInputPrompt("Addressbook Release V15.0- LokeshDayal website");
         filter.addTextChangeListener(e -> refreshContacts(e.getText()));
 
         contactList
